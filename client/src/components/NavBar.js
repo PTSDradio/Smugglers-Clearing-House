4a31a17415355efd12799ea18b99af76b05c4ab2
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-08-31 09:30:33 -0500

    revert changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,17 @@
 import { NavLink } from "react-router-dom";
-import logo2 from "./images/logo2.png"
+import logo1 from "./images/logo1.png"
 
 function NavBar() {
   
   return (
     <header>      
         <nav className="navbar-styles"> 
-          <img className="logo2-img" src={logo2} alt={"logo"}/>
-          <li> <NavLink to='/'> Buy </NavLink> </li>
-          <li> <NavLink to="/sell"> Sell </NavLink> </li>
-          <li> <NavLink to='/account'> Account </NavLink> </li>
+          <img className="logo1-img" src={logo1} alt={"logo"}/>
+          <div>
+            <li> <NavLink to='/'> Buy </NavLink> </li>
+            <li> <NavLink to="/sell"> Sell </NavLink> </li>
+            <li> <NavLink to='/account'> Account </NavLink> </li>
+          </div>
         </nav>
     </header>
   );
