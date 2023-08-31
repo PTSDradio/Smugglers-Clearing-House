69ac9357efd64d1ec2e58fe114a4e3758f3319b9
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-08-30 22:34:11 -0500

    dumb af logo

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
+import logo2 from "./images/logo2.png"
 
 function NavBar() {
   
   return (
     <header>      
         <nav className="navbar-styles"> 
-          <h1>Smuggler's Clearing House</h1>  
+          <img className="logo2-img" src={logo2} alt={"logo"}/>
           <li> <NavLink to='/'> Buy </NavLink> </li>
           <li> <NavLink to="/sell"> Sell </NavLink> </li>
           <li> <NavLink to='/account'> Account </NavLink> </li>
